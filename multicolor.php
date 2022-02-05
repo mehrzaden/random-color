0561288cfacf9aca1f10482e50d305e30f1ec70a
mehrzaden <36074227+mehrzaden@users.noreply.github.com>
2022-02-05 12:15:23 +0330

    first commit

---
+++ multicolor.php
@@ -0,0 +1,29 @@
+<?php
+
+function generate(){
+    echo '<table style="width:100%; height: 100%;">' .PHP_EOL;
+            for ($i = 0; $i <=2 ; $i++) {
+                echo '<tr>' .PHP_EOL;
+                for ($j = 0; $j<= 2 ; $j++) {
+                    echo "<td style='background-color: rgb(".rand(0,255).", ".rand(0,255).", ".rand(0,255).")'> </td>" .PHP_EOL; ;
+
+                }
+                echo "</tr>" .PHP_EOL;
+            }
+            echo "</table>" .PHP_EOL;
+
+            echo "test";
+
+
+ 
+
+
+
+}
+
+generate();
+ echo "aaaa";
+ header("Refresh:0");
+
+ 
+ 
